228ec4436d6ca99cea4f7dcfbf515793cebba8ed
Keisuke Kobayashi <b1218886@planet.kanazawa-it.ac.jp>
2015-11-01 21:18:34 +0900

    Initial Commit

---
+++ OldMapAppsSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OldMapAppsSample
+//
+//  Created by Keisuke.K on 2015/11/01.
+//  Copyright © 2015年 Keisuke.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
