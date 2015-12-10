459acb411dc2a6fd22f30f855f178e6254d659f8
Keisuke Kobayashi <b1218886@planet.kanazawa-it.ac.jp>
2015-12-11 05:04:54 +0900

    編集完了した古地図を追加した

--- OldMapAppsSample/ViewController.swift
+++ OldMapAppsSample/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController,GMSMapViewDelegate {
     //緯度経度 -> 香林坊
     let lat: CLLocationDegrees = 36.562466
     let lon: CLLocationDegrees = 136.653596
+
     
     //切り替えボタンの作成
     let chengeButton: UIButton = UIButton()
@@ -60,6 +61,47 @@ class ViewController: UIViewController,GMSMapViewDelegate {
         marker.position = CLLocationCoordinate2DMake(lat, lon)
         marker.map = googleMap
         
+        /*
+        ここからmapの調節で使用するマーカー
+        */
+        let l2: CLLocationDegrees = 36.571837
+        let lo2: CLLocationDegrees = 136.657223
+        let m2: GMSMarker = GMSMarker()
+        m2.position = CLLocationCoordinate2DMake(l2, lo2)
+        m2.map = googleMap
+        
+        let l3: CLLocationDegrees = 36.572669
+        let lo3: CLLocationDegrees = 136.658648
+        let m3: GMSMarker = GMSMarker()
+        m3.position = CLLocationCoordinate2DMake(l3, lo3)
+        m3.map = googleMap
+        
+        let l4: CLLocationDegrees = 36.572326
+        let lo4: CLLocationDegrees = 136.661357
+        let m4: GMSMarker = GMSMarker()
+        m4.position = CLLocationCoordinate2DMake(l4, lo4)
+        m4.map = googleMap
+        
+        let l5: CLLocationDegrees = 36.560295
+        let lo5: CLLocationDegrees = 136.656937
+        let m5: GMSMarker = GMSMarker()
+        m5.position = CLLocationCoordinate2DMake(l5, lo5)
+        m5.map = googleMap
+
+        let l6: CLLocationDegrees = 36.565506
+        let lo6: CLLocationDegrees = 136.665878
+        let m6: GMSMarker = GMSMarker()
+        m6.position = CLLocationCoordinate2DMake(l6, lo6)
+        m6.map = googleMap
+        
+        
+        let L1C1_la: CLLocationDegrees = 36.577517
+        let L1C1_lo: CLLocationDegrees = 136.649726
+        let MapL1C1: GMSMarker = GMSMarker()
+        MapL1C1.position = CLLocationCoordinate2DMake(L1C1_la, L1C1_lo)
+        MapL1C1.map = googleMap
+        
+        
         //古地図の表示
         overLay()
         
@@ -69,22 +111,19 @@ class ViewController: UIViewController,GMSMapViewDelegate {
     
     
     func overLay(){
-        
-        //var southWest = CLLocationCoordinate2DMake(36.549910, 136.632714);
-        //var northEast = CLLocationCoordinate2DMake(36.582960, 136.683321);
-        var southWest = CLLocationCoordinate2DMake(36.583648, 136.681735);
-        var northEast = CLLocationCoordinate2DMake(36.550341, 136.631738);
+       
+        //基準
+        var southWest = CLLocationCoordinate2DMake(36.555973, 136.649793);
+        var northEast = CLLocationCoordinate2DMake(36.576272, 136.668853);
         
         var overlayBounds = GMSCoordinateBounds(coordinate: southWest, coordinate: northEast)
         
-        var icon = UIImage(named: "sample.jpg")
+        var icon = UIImage(named: "map.png")
         
         //mapプロパティにGMSMapViewをセットする
         overlay = GMSGroundOverlay(bounds: overlayBounds, icon: icon)
         overlay.bearing = 0
         overlay.map = googleMap
-        print("aaa")
-        print(overlay)
         
     }
     
@@ -110,7 +149,6 @@ class ViewController: UIViewController,GMSMapViewDelegate {
     
     //ボタンを押している状態の処理
     func moveClickChengeButton(){
-        //self.nextButton.backgroundColor = UIColor.blackColor()
         self.chengeButton.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.8)
     }
     
@@ -120,7 +158,7 @@ class ViewController: UIViewController,GMSMapViewDelegate {
         //flg == 0なら古地図を消す
         if(flg == 0){
             overlay.map = nil;
-            print("現代MAPに切り替えました")
+            //print("現代MAPに切り替えました")
             flg = 1
         }
         else {
@@ -134,7 +172,7 @@ class ViewController: UIViewController,GMSMapViewDelegate {
     }
     
     func outClickChengeButton(){
-        print("ずれました")
+        //print("ずれました")
     }
     
     
